fee375db506d25d49a6e6447ab55d933836a086c
Thomas Nairn <tom.nairn@iiincubator.com>
2017-05-14 18:52:46 +0100

    Initial commit

---
+++ fuse.js
@@ -0,0 +1,32 @@
+const { FuseBox, WebIndexPlugin, CSSPlugin, CSSResourcePlugin, SassPlugin, JSONPlugin, HTMLPlugin, TypeScriptHelpers } = require("fuse-box");
+
+const fuse = FuseBox.init({
+  homeDir: "src",
+  output: "build/$name.js",
+  plugins: [
+    WebIndexPlugin({
+      template: "src/index.html"
+    }),
+    [
+      SassPlugin({ outputStyle: "compressed" }),
+      CSSPlugin()
+    ],
+    [
+      CSSResourcePlugin({
+        inline: true
+      }), CSSPlugin()
+    ],
+    TypeScriptHelpers(),
+    JSONPlugin(),
+    HTMLPlugin({ useDefault: false })
+  ]
+});
+
+fuse.dev({ port: 8100 });
+
+fuse.bundle("app")
+  .instructions("> main.ts")
+  .watch()
+  .hmr()
+
+fuse.run();
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Facebook } from '@ionic-native/facebook';
+class TestClass {
+    constructor() {
+
+    }
+
+    doThis() {
+        console.log(Facebook)
+    }
+}
